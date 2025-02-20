9dc4d50993443fa5d3f4a5fd653454aaf4eaef4b
jairaws <167156521+jairaws@users.noreply.github.com>
2025-02-20 15:14:50 -0800

    fix: Add length validations for job settings input fields (#86)
    Signed-off-by: Jair Ruiz <167156521+jairaws@users.noreply.github.com>

--- src/deadline/max_submitter/ui/scene_settings_tab.py
+++ src/deadline/max_submitter/ui/scene_settings_tab.py
@@ -39,9 +39,6 @@ from deadline.max_submitter.utilities import max_utils
 
 _logger = logging.getLogger(__name__)
 
-# This is the maximum string length allowed for OJD job parameters.
-DEFAULT_MAX_STRING_LENGTH: int = 1024
-
 
 class FileSearchLineEdit(QWidget):
     """
@@ -62,7 +59,6 @@ class FileSearchLineEdit(QWidget):
         lyt.setMargin(0)
 
         self.edit = QLineEdit(self)
-        self.edit.setMaxLength(DEFAULT_MAX_STRING_LENGTH)
         self.btn = QPushButton("...", parent=self)
         self.btn.setMaximumSize(QSize(100, 40))
         self.btn.clicked.connect(self.get_file)
@@ -134,7 +130,6 @@ class SceneSettingsWidget(QWidget):
 
         # Project path
         self.proj_path_txt = QLineEdit(self)
-        self.proj_path_txt.setMaxLength(DEFAULT_MAX_STRING_LENGTH)
         self.proj_path_txt.setEnabled(False)
         lyt.addWidget(QLabel("Project Path"), 0, 0)
         lyt.addWidget(self.proj_path_txt, 0, 1)
@@ -146,7 +141,6 @@ class SceneSettingsWidget(QWidget):
 
         # Output filename
         self.output_name_txt = QLineEdit(self)
-        self.output_name_txt.setMaxLength(DEFAULT_MAX_STRING_LENGTH)
         lyt.addWidget(QLabel("Output Filename"), 2, 0)
         lyt.addWidget(self.output_name_txt, 2, 1)
 
@@ -215,7 +209,6 @@ class SceneSettingsWidget(QWidget):
         # Override frame range
         self.frame_override_chck = QCheckBox("Override Frame Range", self)
         self.frame_override_txt = QLineEdit(self)
-        self.frame_override_txt.setMaxLength(DEFAULT_MAX_STRING_LENGTH)
         self.frame_override_txt.setToolTip(
             "Frame range you want to use as override. \n" "E.g. 1,3,5-10 or 1, 3, 5-10"
         )
---
+++ test/unit/deadline_submitter_for_3ds_max/test_sanity_checks.py
@@ -0,0 +1,151 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+import random
+import string
+
+from unittest.mock import patch, Mock
+from pytest import fixture, raises
+
+from deadline.max_submitter.sanity_checks import (
+    check_sanity,
+    JOB_PARAMETER_MAX_STRING_LENGTH,
+    STEP_NAME_MAX_STRING_LENGTH,
+    ALL_CAMERAS_STR,
+    ALL_STATE_SETS_STR,
+    ALLOWED_RENDERERS,
+)
+from deadline.max_submitter.data_classes import RenderSubmitterUISettings
+
+
+@fixture(scope="session", autouse=True)
+def mock_max_utils():
+    with patch("deadline.max_submitter.sanity_checks.max_utils") as mock_max_utils:
+        mock_max_utils.get_camera_names.return_value = [
+            create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH)
+        ]
+        mock_max_utils.get_state_set_names.return_value = [
+            [
+                create_random_string(STEP_NAME_MAX_STRING_LENGTH),
+                create_random_string(STEP_NAME_MAX_STRING_LENGTH),
+            ]
+        ]
+        mock_max_utils.is_correct_frame_range.return_value = True
+        mock_max_utils.get_duplicate_frames.return_value = False
+        yield mock_max_utils
+
+
+@fixture(scope="session", autouse=True)
+def mock_pymx_runtime():
+    with patch("deadline.max_submitter.sanity_checks.rt") as mock_pymx_runtime:
+        mock_pymx_runtime.renderers.current = ALLOWED_RENDERERS[0]
+        yield mock_pymx_runtime
+
+
+@fixture
+def default_settings() -> RenderSubmitterUISettings:
+    settings: RenderSubmitterUISettings = RenderSubmitterUISettings()
+    settings.name = "test_job_name"
+    settings.project_path = create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH)
+    settings.output_path = create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH)
+    settings.output_name = create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH)
+    settings.camera_selection = ALL_CAMERAS_STR
+    settings.state_set = ALL_STATE_SETS_STR
+    settings.override_frame_range = True
+    settings.frame_list = create_frame_range(JOB_PARAMETER_MAX_STRING_LENGTH)
+
+    return settings
+
+
+def create_random_string(length: int) -> str:
+    return "".join(random.choice(string.ascii_letters) for _ in range(length))
+
+
+def create_frame_range(length: int) -> str:
+    frame_range: str = ""
+    iteration: int = 0
+    while len(frame_range) <= length:
+        frame_range += f"{str(iteration)},"
+
+    frame_range = frame_range[:-1]
+
+    if len(frame_range) > length:
+        frame_range = frame_range[: frame_range.rfind(",")]
+
+    return frame_range
+
+
+class TestSanityChecks:
+
+    def test_check_sanity_project_path_length(
+        self,
+        default_settings: RenderSubmitterUISettings,
+    ) -> None:
+        check_sanity(default_settings)
+
+        default_settings.project_path = create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH + 1)
+
+        with raises(Exception):
+            check_sanity(default_settings)
+
+    def test_check_sanity_output_path_length(
+        self,
+        default_settings: RenderSubmitterUISettings,
+    ) -> None:
+        check_sanity(default_settings)
+
+        default_settings.output_path = create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH + 1)
+
+        with raises(Exception):
+            check_sanity(default_settings)
+
+    def test_check_sanity_output_file_name_length(
+        self,
+        default_settings: RenderSubmitterUISettings,
+    ) -> None:
+        check_sanity(default_settings)
+
+        default_settings.output_name = create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH + 1)
+
+        with raises(Exception):
+            check_sanity(default_settings)
+
+    def test_check_sanity_state_set_name_length(
+        self,
+        mock_max_utils: Mock,
+        default_settings: RenderSubmitterUISettings,
+    ) -> None:
+        check_sanity(default_settings)
+
+        mock_max_utils.get_state_set_names.return_value = [
+            [
+                create_random_string(STEP_NAME_MAX_STRING_LENGTH + 1),
+                create_random_string(STEP_NAME_MAX_STRING_LENGTH + 1),
+            ]
+        ]
+
+        with raises(Exception):
+            check_sanity(default_settings)
+
+    def test_check_sanity_camera_name_length(
+        self,
+        default_settings: RenderSubmitterUISettings,
+        mock_max_utils: Mock,
+    ) -> None:
+        check_sanity(default_settings)
+
+        mock_max_utils.get_camera_names.return_value = [
+            create_random_string(JOB_PARAMETER_MAX_STRING_LENGTH + 1)
+        ]
+
+        with raises(Exception):
+            check_sanity(default_settings)
+
+    def test_check_frame_range_length(
+        self,
+        default_settings: RenderSubmitterUISettings,
+    ) -> None:
+        check_sanity(default_settings)
+
+        default_settings.frame_list = create_frame_range(JOB_PARAMETER_MAX_STRING_LENGTH + 1)
+
+        with raises(Exception):
+            check_sanity(default_settings)

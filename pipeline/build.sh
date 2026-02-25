f394592af4dc04e6baaa56572e34210b61956ec4
Jericho Tolentino <68654047+jericht@users.noreply.github.com>
2026-02-25 15:57:54 -0800

    chore: temporary pin virtualenv<21 to fix build (#206)
    Signed-off-by: Jericho Tolentino <68654047+jericht@users.noreply.github.com>

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 pip install --upgrade pip
-pip install --upgrade hatch "click<8.3"
+pip install --upgrade hatch "click<8.3" "virtualenv<21"
 pip install --upgrade twine
 hatch run lint
 hatch run test
